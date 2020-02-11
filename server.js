ac6aaa681b52a619a95adc96b170fd39339294b5
Jonathan Fox <FoxJonathanP@gmail.com>
2020-02-11 12:05:45 -0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var socketIO = require("socket.io");
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+// Routing
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
+
+// Add the WebSocket handlers
+io.on("connection", function(socket) {
+
+});
+
+const players = {};
+
+// GAME LOOP
+setInterval(function() {
+  Object.values(players).forEach((player) => {
+    let xMove = 0;
+    let yMove = 0;
+    let moveSpeed = 5;
+    if (player.inputs.up) yMove--;
+    if (player.inputs.down) yMove++;
+    if (player.inputs.left) xMove--;
+    if (player.inputs.right) xMove++;
+    player.position.x += xMove*moveSpeed;
+    player.position.y += yMove*moveSpeed;
+  });
+
+  io.sockets.emit("players", players);
+}, 1000/60);
+
+io.on("connection", function(socket) {
+  socket.on("new player", function() {
+    players[socket.id] = {
+      inputs: {},
+      position: {x:300, y:300}
+    };
+  });
+  socket.on("inputs", function(inputs) {
+    let player = players[socket.id];
+    if (player) player.inputs = inputs;
+  })
+})
---
+++ static/game.js
@@ -0,0 +1,51 @@
+var socket = io();
+socket.on("message", function(data) {
+  console.log(data);
+})
+
+var keyMap = {
+  87: 'up',
+  83: 'down',
+  65: 'left',
+  68: 'right'
+}
+
+var inputs = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+}
+
+document.addEventListener("keydown", function(e) {
+  inputs[keyMap[e.keyCode]] = true;
+  console.log(inputs);
+  socket.emit("inputs", inputs);
+});
+
+document.addEventListener("keyup", function(e) {
+  inputs[keyMap[e.keyCode]] = false;
+  console.log(inputs);
+  socket.emit("inputs", inputs);
+})
+
+socket.emit('new player');
+
+var canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+var context = canvas.getContext("2d");
+socket.on("players", function(players) {
+  context.clearRect(0, 0, 800, 600);
+  context.fillStyle='green';
+  for (var id in players) {
+    var player = players[id];
+    context.beginPath();
+    context.arc(
+      player.position.x, 
+      player.position.y,
+      10, 0, 2 * Math.PI
+    );
+    context.fill();
+  }
+})
