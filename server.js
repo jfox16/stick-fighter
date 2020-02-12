67b6895c253a9bd80e7cc77d2bca5c2072a3c8ec
Jonathan Fox <FoxJonathanP@gmail.com>
2020-02-12 15:32:27 -0800

    added ping

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 var app = express();
 var server = http.Server(app);
-var io = socketIO(server, {pingInterval: 5000});
+var io = socketIO(server, {pingInterval: 1000});
 
 app.set('port', PORT);
 app.use('/static', express.static(__dirname + '/static'));
